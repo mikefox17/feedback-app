ccbfecf3b8b46ac752e27fc5ff697d59e888bb52
mikefox17 <mikefox17@gmail.com>
2021-12-27 11:16:15 -0500

    framer motion added, add feedback and push to state added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData.js';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
+
     const deleteFeedback = id => {
         if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter(item => item.id !== id));
         }
     };
+
+    const addFeedback = newFeedback => {
+        newFeedback.id = uuidv4();
+        console.log(newFeedback);
+        setFeedback([newFeedback, ...feedback]);
+    };
+
     return (
         <>
             <Header text='Feedback App' />
             <div className='App container'>
+                <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList
                     feedback={feedback}
